0fa808c55f7f875da7020f9cc9c8088dc8f82fb7
ohnas <ohnas12@gmail.com>
2023-04-05 12:10:39 +0900

    1.4 Layout System

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <View style={{ flex:1, backgroundColor: "tomato"}}></View>
+      <View style={{ flex:1, backgroundColor: "teal"}}></View>
+      <View style={{ flex:1, backgroundColor: "orange"}}></View>
       <StatusBar style='dark' />
     </View>
   );
@@ -13,12 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color: "black",
+    flexDirection: "row",
   },
 });
