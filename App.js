5a24a6a3d82bd7fbba1db838be6f1b8328dabae1
ohnas <ohnas12@gmail.com>
2023-04-05 15:06:39 +0900

    1.10 Icons

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
+import { Fontisto } from "@expo/vector-icons";
 import { StyleSheet, View, Text, Dimensions, ScrollView, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -34,25 +44,28 @@ export default function App() {
   }, [])
   return (
     <View style={styles.container}>
+      <StatusBar style='light' />
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView contentContainerStyle={styles.weather} pagingEnabled horizontal showsHorizontalScrollIndicator={false}>
         {days.length === 0 ? 
-          <View style={styles.day}>
+          <View style={{...styles.day, alignItems: "center"}}>
             <ActivityIndicator color={'white'} style={{ marginTop: 10 }} size={'large'} />
           </View>
           :
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <View style={{flexDirection: "row", alignItems: "center", width: "100%", justifyContent: "space-between",}}>
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color={'white'} />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
           ))
         }
       </ScrollView>
-      <StatusBar style='dark' />
     </View>
   );
 }
@@ -70,22 +83,30 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 58,
     fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
     marginTop: 50,
     fontWeight: "600",
-    fontSize: 178,
+    fontSize: 100,
+    color: "white",
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500",
   },
   tinyText: {
-    fontSize: 20,
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
   },
 });
